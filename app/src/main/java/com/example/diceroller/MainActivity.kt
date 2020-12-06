40f1b4cc8a5505b842cdf74c67d263aa03990ea3
Vinicius <cerradosoftware@gmail.com>
2020-12-06 13:36:00 -0300

    Inicial

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+
+
+
+
+        }
+}
